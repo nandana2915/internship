e804d4df212300e03e0df4963b4ac55352d60137
nandana2915 <130975174+nandana2915@users.noreply.github.com>
2024-08-15 20:55:25 +0530

    Add files via upload
    question 10.

---
+++ main.java
@@ -0,0 +1,12 @@
+enum Day 
+{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+}
+
+public class Main 
+{
+    public static void main(String[] args) {
+        Day day = Day.WEDNESDAY;
+        System.out.println(day.ordinal()); 
+    }
+}

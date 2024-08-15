0b1022ab307064f2184b2fbe5449401eb24ef3cf
nandana2915 <130975174+nandana2915@users.noreply.github.com>
2024-08-15 20:58:01 +0530

    Update main.java

--- main.java
+++ main.java
@@ -5,7 +5,8 @@ enum Day
 
 public class Main 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Day day = Day.WEDNESDAY;
         System.out.println(day.ordinal()); 
     }

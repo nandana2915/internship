187865c980855104ba14c897e9b9ebdf986def7f
nandana2915 <130975174+nandana2915@users.noreply.github.com>
2024-09-05 22:33:04 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,13 +1,22 @@
-enum Day 
-{
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
-}
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Main 
 {
     public static void main(String[] args) 
     {
-        Day day = Day.WEDNESDAY;
-        System.out.println(day.ordinal()); 
+        
+        List<Integer> numbers = Arrays.asList(1, 34, 22, 13, 55, 4, 3, 87, 11, 22, 12);
+        
+        
+        List<Integer> filteredAndSorted = numbers.stream()
+                .filter(num -> num % 2 == 0)   
+                .sorted()                      
+                .collect(Collectors.toList());
+
+        
+        System.out.println("Filtered and Sorted List: " + filteredAndSorted);
     }
 }
